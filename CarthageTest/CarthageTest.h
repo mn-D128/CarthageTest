9b4bf3594190d19a214fab05fbe52dba88c5bb85
mn(D128) <>
2018-03-26 15:37:16 +0900

    プロジェクト追加

---
+++ CarthageTest/CarthageTest.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTest.h
+//  CarthageTest
+//
+//  Created by mn(D128) on 2018/03/25.
+//  Copyright © 2018年 mn(D128). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageTest.
+FOUNDATION_EXPORT double CarthageTestVersionNumber;
+
+//! Project version string for CarthageTest.
+FOUNDATION_EXPORT const unsigned char CarthageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTest/PublicHeader.h>
+
+
---
+++ CarthageTest/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  CarthageTest
+//
+//  Created by mn(D128) on 2018/03/25.
+//  Copyright © 2018年 mn(D128). All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    
+    public class var output: String {
+        return "output"
+    }
+    
+}
